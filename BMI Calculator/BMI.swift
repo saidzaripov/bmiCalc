7cde7e2f392228831336476f66049e78b7dcb1ba
saidzaripov <said.zaripov@icloud.com>
2024-11-12 10:37:37 -0500

    completed

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Said Zaripov on 2024-11-08.
+//
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMI Calculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Said Zaripov on 2024-11-07.
+//
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
